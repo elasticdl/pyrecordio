be8fa35beddd6dc5c4559d053d9cb50d0f519b15
Li Zou <zou000@gmail.com>
2018-12-05 12:03:44 -0800

    * add setup script

---
+++ setup.py
@@ -0,0 +1,21 @@
+# Install recordio package by setup.py
+# Local installation:
+#     pip3 install .
+# Make package:
+#     python3 setup.py bdist
+
+from setuptools import setup, find_packages
+
+packages = find_packages(where='.', exclude=['*.pyc'])
+
+install_requires=['crc32c']
+
+setup(name='recordio',
+      version='0.0.1',
+      description='recordio file format support',
+      url='https://github.com/ElasticDL/pyrecordio',
+      author='Ant',
+      author_email='XXX@antfin.com',
+      license='TBD',
+      packages=packages,
+      install_requires=install_requires)
